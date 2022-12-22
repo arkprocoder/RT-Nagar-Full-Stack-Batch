47d29daf9a5e3c8c4df52356bdad71c7dcb4efbd
arkprocoder <aneesurrehman423@gmail.com>
2022-12-22 17:52:57 +0530

    filess

---
+++ PYTHONNN/basics/pgm17.py
@@ -0,0 +1,73 @@
+# f=open('data.txt')
+# print(type(f))
+# content=f.read()
+# print(type(content))
+# print(content)
+# f.close()
+
+
+# f=open('data.txt')
+# content=f.read(5)
+# content=f.read(25)
+# print(content)
+# f.close()
+
+# f=open('data.txt')
+# content=f.readline()
+# print(content)
+# content=f.readline()
+# print(content)
+# f.close()
+
+# f=open('data.txt')
+# f.readline()
+# f.readline()
+# content=f.readline()
+# print(content)
+# f.close()
+
+# f=open('data.txt')
+# print(f)
+# content=f.readlines()
+# print(content[0])
+# print(content[2])
+# f.close()
+
+# f=open('data.txt','rt')
+# print(f)
+# content=f.read()
+# for i in  content:
+#     print(i)
+# f.close()
+
+# f=open('data.txt','rt')
+# content=f.readlines()
+
+# for i in  content[::2]:
+#     print(i)
+# f.close()
+
+# f=open('ark.txt','w')
+# f.write('Pathaan is an upcoming Indian Hindi-language action thriller film.\nDirected by Siddharth Anand from a screenplay by Shridhar Raghavan and a story by Anand.\nProduced by Aditya Chopra for Yash Raj Films, it is the fourth installment in the YRF Spy Universe and stars.\nShah Rukh Khan, John Abraham, and Deepika Padukone.')
+# f.close()
+
+# f=open('ark.txt','w')
+# f.write('anees')
+# f.close()
+
+# f=open('quick.txt','x')
+# f.write('\ni will watch this movie..')
+# f.close()
+
+# f=open('ark.txt','r+')
+# content=f.read()
+# print(content)
+# f.write('rehman\n')
+# f.close()
+
+# print("after updating the new content")
+# f=open('ark.txt','r+')
+# content=f.read()
+# print(content)
+# f.close()
+
